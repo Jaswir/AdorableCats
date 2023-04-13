75dc31e5e8f79dd9a3454d83c7cf21089f939fb8
Jaswir <jaswirraghoe@gmail.com>
2023-04-13 15:02:55 +0200

    Standalone aspnet server before docker

--- AdorableCats/Data/Seeder.cs
+++ AdorableCats/Data/Seeder.cs
@@ -9,7 +9,8 @@ namespace AdorableCats.Data
         public async void Seed(CatDbContext catDbContext)
         {
             //Reads in CatImage Data from json file 
-            string cat_imagesJSON = System.IO.File.ReadAllText(@"Data\cat_images_without_dupes.json");
+            var path = Path.Combine(Environment.CurrentDirectory, "Data\\cat_images_without_dupes.json");
+            string cat_imagesJSON = System.IO.File.ReadAllText(path);
             List<CatImage> catImagesList = JsonConvert.DeserializeObject<List<CatImage>>(cat_imagesJSON);
 
             //Seeds CatImage Data incase table is empty
--- AdorableCats/Program.cs
+++ AdorableCats/Program.cs
@@ -54,10 +54,6 @@ app.UseStaticFiles();
 app.UseRouting();
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html"); ;
+app.MapControllers();
 
 app.Run();
